960ef5faff2fa6f7eb918742db9e30dac44897f7
Emiliandro Firmino <ecdmf.tjd@gmail.com>
2017-06-05 00:04:07 -0700

    Scene Vinheta

---
+++ Assets/Textures/Vinheta/Vinheta.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Vinheta : MonoBehaviour
+{
+	/**
+	pelo inspector dite o tempo necessario para carregar a proxima cena
+	e também dite o nome da proxima cena
+	 */
+	 
+    [SerializeField] private string _cena;
+    [SerializeField] private float _tempo;
+    // Use this for initialization
+    void Start()
+    {
+        Invoke("ChangeSceneAfter", _tempo * Time.deltaTime);
+    }
+
+    // Update is called once per frame
+    void ChangeSceneAfter()
+    {
+        SceneManager.LoadScene(_cena);
+
+    }
+}
